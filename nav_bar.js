e5e0c9902bfd799b700392f729d22c57324fe95b
hankxxxxx6-ops <hankxxxxx6@gmail.com>
2026-05-05 17:18:12 +0800

    Add files via upload

---
+++ nav_bar.js
@@ -0,0 +1,16 @@
+
+
+        window.onload = function(){
+            document.getElementById("nav_bar").innerHTML=`<nav>
+        <div class="container">
+            <a href="index.html" class="logo">貝貝之旅</a>
+            <ul>
+                <li><a href="index.html">首頁</a></li>
+                <li><a href="about.html">LIVLY</a></li>
+                <li><a href="courses.html">密室逃脫</a></li>
+                <li><a href="tools.html">動畫</a></li>
+                <li><a href="testimonials.html">拼圖</a></li>
+            </ul>
+        </div>
+    </nav>`;
+        }
